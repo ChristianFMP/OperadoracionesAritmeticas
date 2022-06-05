f9d9eecbde969379ba5495e44085739b40f97bfc
ChristianFMP <171565@unsaac.edu.pe>
2022-06-05 16:22:17 -0500

    Implementacion del menu de opciones

--- OperacionesAritmeticas.py
+++ OperacionesAritmeticas.py
@@ -1,4 +1,17 @@
 # Programa Principal
 # Menu de opciones
 def MenuOpciones():
-    pass 
+    print("HOLA GRACIAS POR USAR NUESTRO SOFTWARE")
+    print("MARQUE LA OPCIÓN QUE DESEA HACER")
+    print("Presione 1 para sumar")
+    print("Presione 2 para restar")
+    print("Presione 3 para multiplicar")
+    print("Presione 4 para dividir")
+    print("Presione 5 para salir")
+    opcion = int(input("Por favor ingrese su opción: "))
+    while((opcion < 1) or (opcion > 5 )):
+        print("Por favor ingrese una opción válida del menú")
+        opcion = int(input("Ingrese nuevamente su opción: "))
+    print("El resultado de su operación es: ")
+    
+MenuOpciones()
