a188990e45c595d2734acd4e4e537929b8558d57
ChristianFMP <171565@unsaac.edu.pe>
2022-06-06 06:05:31 -0500

    Añadiendo informacion al modulo de la multiplicacion

--- Modulos/Multiplicacion.py
+++ Modulos/Multiplicacion.py
@@ -2,3 +2,8 @@ def Multiplicacion(x, y):
   return(x*y)
 print("Gracias por usar nuestro modulo de la multiplicación")
 print("Modulo Multiplicación")
+
+def Multiplicar(x, y):
+  return(x*y)
+print("gracias por usar nuestro modulo de multiplicacion")
+print("Modulo Multiplicacion")
